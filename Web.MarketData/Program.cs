e9b1cb7aebd5808cdcb93c8d21c98c209c5b5fb3
Marcelo <omarcelo.0101@gmail.com>
2026-09-17 23:24:31 -0300

    Add MarketData configuration and storage provider selection

--- Web.MarketData/Program.cs
+++ Web.MarketData/Program.cs
@@ -1,3 +1,6 @@
+using Domain.MarketData.Business.Interfaces;
+using Infrastructure.MarketData.Options;
+using Infrastructure.MarketData.Persistence;
 
 namespace Web.MarketData;
 
@@ -14,6 +17,24 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+
+        builder.Services.Configure<StorageOptions>(builder.Configuration.GetSection("MarketData:Storage"));
+        builder.Services.Configure<DllCredentialsOptions>(builder.Configuration.GetSection("MarketData:DllCredentials"));
+
+        var storageProvider = builder.Configuration["MarketData:Storage:Provider"] ?? "Parquet";
+        if (string.Equals(storageProvider, "Csv", StringComparison.OrdinalIgnoreCase))
+        {
+            builder.Services.AddSingleton<ITradeTickRepository, CsvTradeTickRepository>();
+        }
+        else
+        {
+            builder.Services.AddSingleton<ITradeTickRepository, ParquetTradeTickRepository>();
+        }
+
+        // TODO: register MarketDataWorker + IMarketDataProvider once IMarketDataProvider
+        // has a concrete implementation (deferred - see Domain.DLL/Domain.Spode gap).
+        // Registering AddHostedService<MarketDataWorker>() before IMarketDataProvider
+        // exists would crash the app at startup under ValidateOnBuild.
 
         var app = builder.Build();
 
